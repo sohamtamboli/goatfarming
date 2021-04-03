61b73ce03a7193b00f82d3d51dd5be9d336c6da1
Pruthvi Shetty <pruthvishetty5656@gmail.com>
2021-04-03 22:31:15 +0530

    Fixes to the about page collapser

--- components/PlanCard.vue
+++ components/PlanCard.vue
@@ -53,139 +53,134 @@
           </v-list>
 
           <v-card-actions>
-            <v-btn color="orange lighten-2" text @click="show.idx = !show.idx">
-              Explore
-            </v-btn>
-
-            <v-spacer></v-spacer>
-
-            <v-btn icon @click="toggleShow(idx)">
-              <v-icon>{{
-                show.idx ? 'mdi-chevron-up' : 'mdi-chevron-down'
-              }}</v-icon>
-            </v-btn>
+            <v-expansion-panels flat>
+              <v-expansion-panel>
+                <v-expansion-panel-header>
+                  Explore <v-spacer />
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <div>
+                    <v-divider></v-divider>
+                    <v-container fluid>
+                      <v-expansion-panels multiple>
+                        <v-expansion-panel>
+                          <v-expansion-panel-header
+                            >Business Plan</v-expansion-panel-header
+                          >
+                          <v-expansion-panel-content>
+                            <v-row>
+                              <v-col
+                                v-for="(item, i) in businessData"
+                                :key="i"
+                                cols="12"
+                              >
+                                <v-list dense>
+                                  <v-list-item>
+                                    <v-list-item-content>
+                                      {{ item.selfInvest }}:
+                                    </v-list-item-content>
+                                    <v-list-item-content class="align-end">
+                                      {{ item.selfInvestValue }}
+                                    </v-list-item-content>
+                                  </v-list-item>
+                                  <v-list-item>
+                                    <v-list-item-content>
+                                      {{ item.debt }}:
+                                    </v-list-item-content>
+                                    <v-list-item-content class="align-end">
+                                      {{ item.debtValue }}
+                                    </v-list-item-content>
+                                  </v-list-item>
+                                  <v-list-item>
+                                    <v-list-item-content>
+                                      {{ item.projectCost }}:
+                                    </v-list-item-content>
+                                    <v-list-item-content class="align-end">
+                                      {{ item.projectCostValue }}
+                                    </v-list-item-content>
+                                  </v-list-item>
+                                  <v-list-item>
+                                    <v-list-item-content>
+                                      {{ item.note }}
+                                    </v-list-item-content>
+                                  </v-list-item>
+                                </v-list>
+                              </v-col>
+                            </v-row>
+                          </v-expansion-panel-content>
+                        </v-expansion-panel>
+                        <v-expansion-panel>
+                          <v-expansion-panel-header
+                            >Expenses</v-expansion-panel-header
+                          >
+                          <v-expansion-panel-content>
+                            <v-row>
+                              <v-col
+                                v-for="(item, i) in expensesData"
+                                :key="i"
+                                cols="12"
+                              >
+                                <v-list dense>
+                                  <v-list-item>
+                                    <v-list-item-content>
+                                      {{ item.construction }}:
+                                    </v-list-item-content>
+                                    <v-list-item-content class="align-end">
+                                      {{ item.constructionValue }}
+                                    </v-list-item-content>
+                                  </v-list-item>
+                                  <v-list-item>
+                                    <v-list-item-content>
+                                      {{ item.playingCapital }}:
+                                    </v-list-item-content>
+                                    <v-list-item-content class="align-end">
+                                      {{ item.playingCapitalValue }}
+                                    </v-list-item-content>
+                                  </v-list-item>
+                                  <v-list-item>
+                                    <v-list-item-content>
+                                      {{ item.buyGoat }}:
+                                    </v-list-item-content>
+                                    <v-list-item-content class="align-end">
+                                      {{ item.buyGoatValue }}
+                                    </v-list-item-content>
+                                  </v-list-item>
+                                  <v-list-item>
+                                    <v-list-item-content>
+                                      {{ item.govTax }}:
+                                    </v-list-item-content>
+                                    <v-list-item-content class="align-end">
+                                      {{ item.govTaxValue }}
+                                    </v-list-item-content>
+                                  </v-list-item>
+                                  <v-list-item>
+                                    <v-list-item-content>
+                                      {{ item.companyFee }}:
+                                    </v-list-item-content>
+                                    <v-list-item-content class="align-end">
+                                      {{ item.companyFeeValue }}
+                                    </v-list-item-content>
+                                  </v-list-item>
+                                  <v-list-item>
+                                    <v-list-item-content>
+                                      {{ item.agencyFee }}:
+                                    </v-list-item-content>
+                                    <v-list-item-content class="align-end">
+                                      {{ item.agencyFeeValue }}
+                                    </v-list-item-content>
+                                  </v-list-item>
+                                </v-list>
+                              </v-col>
+                            </v-row>
+                          </v-expansion-panel-content>
+                        </v-expansion-panel>
+                      </v-expansion-panels>
+                    </v-container>
+                  </div>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
           </v-card-actions>
-
-          <v-expand-transition>
-            <div v-show="shows">
-              <v-divider></v-divider>
-              <v-container fluid>
-                <v-expansion-panels multiple>
-                  <v-expansion-panel>
-                    <v-expansion-panel-header
-                      >Business Plan</v-expansion-panel-header
-                    >
-                    <v-expansion-panel-content>
-                      <v-row>
-                        <v-col
-                          v-for="(item, i) in businessData"
-                          :key="i"
-                          cols="12"
-                        >
-                          <v-list dense>
-                            <v-list-item>
-                              <v-list-item-content>
-                                {{ item.selfInvest }}:
-                              </v-list-item-content>
-                              <v-list-item-content class="align-end">
-                                {{ item.selfInvestValue }}
-                              </v-list-item-content>
-                            </v-list-item>
-                            <v-list-item>
-                              <v-list-item-content>
-                                {{ item.debt }}:
-                              </v-list-item-content>
-                              <v-list-item-content class="align-end">
-                                {{ item.debtValue }}
-                              </v-list-item-content>
-                            </v-list-item>
-                            <v-list-item>
-                              <v-list-item-content>
-                                {{ item.projectCost }}:
-                              </v-list-item-content>
-                              <v-list-item-content class="align-end">
-                                {{ item.projectCostValue }}
-                              </v-list-item-content>
-                            </v-list-item>
-                            <v-list-item>
-                              <v-list-item-content>
-                                {{ item.note }}
-                              </v-list-item-content>
-                            </v-list-item>
-                          </v-list>
-                        </v-col>
-                      </v-row>
-                    </v-expansion-panel-content>
-                  </v-expansion-panel>
-                  <v-expansion-panel>
-                    <v-expansion-panel-header
-                      >Expenses</v-expansion-panel-header
-                    >
-                    <v-expansion-panel-content>
-                      <v-row>
-                        <v-col
-                          v-for="(item, i) in expensesData"
-                          :key="i"
-                          cols="12"
-                        >
-                          <v-list dense>
-                            <v-list-item>
-                              <v-list-item-content>
-                                {{ item.construction }}:
-                              </v-list-item-content>
-                              <v-list-item-content class="align-end">
-                                {{ item.constructionValue }}
-                              </v-list-item-content>
-                            </v-list-item>
-                            <v-list-item>
-                              <v-list-item-content>
-                                {{ item.playingCapital }}:
-                              </v-list-item-content>
-                              <v-list-item-content class="align-end">
-                                {{ item.playingCapitalValue }}
-                              </v-list-item-content>
-                            </v-list-item>
-                            <v-list-item>
-                              <v-list-item-content>
-                                {{ item.buyGoat }}:
-                              </v-list-item-content>
-                              <v-list-item-content class="align-end">
-                                {{ item.buyGoatValue }}
-                              </v-list-item-content>
-                            </v-list-item>
-                            <v-list-item>
-                              <v-list-item-content>
-                                {{ item.govTax }}:
-                              </v-list-item-content>
-                              <v-list-item-content class="align-end">
-                                {{ item.govTaxValue }}
-                              </v-list-item-content>
-                            </v-list-item>
-                            <v-list-item>
-                              <v-list-item-content>
-                                {{ item.companyFee }}:
-                              </v-list-item-content>
-                              <v-list-item-content class="align-end">
-                                {{ item.companyFeeValue }}
-                              </v-list-item-content>
-                            </v-list-item>
-                            <v-list-item>
-                              <v-list-item-content>
-                                {{ item.agencyFee }}:
-                              </v-list-item-content>
-                              <v-list-item-content class="align-end">
-                                {{ item.agencyFeeValue }}
-                              </v-list-item-content>
-                            </v-list-item>
-                          </v-list>
-                        </v-col>
-                      </v-row>
-                    </v-expansion-panel-content>
-                  </v-expansion-panel>
-                </v-expansion-panels>
-              </v-container>
-            </div>
-          </v-expand-transition>
         </v-card>
       </div>
     </div>
