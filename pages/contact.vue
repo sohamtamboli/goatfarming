d362de73e7902578ef0943401c8e8b233bb7a257
Soham Tamboli <sohamtamboli@gmail.com>
2021-04-09 21:25:09 +0530

    Added pdf download, Meta tags

--- pages/contact.vue
+++ pages/contact.vue
@@ -55,6 +55,102 @@ export default {
       this.color = 'success'
     })
   },
+  head() {
+    return {
+      title: 'Contact Us - Anjali Goatfarms',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Goat Farming is a business which can be achieved with the help of less investment and also less space. In closed goat farming, the fodder required to feed the goats is given to them in the cowshed. Also, goats require less amount of food compared to other animals.',
+        },
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content:
+            'anjalibusinessgroup, anjali goat farming, anjali business, anjali group, passplus, goat, farming, goatfarming, agro, agriculture,',
+        },
+        {
+          hid: 'author',
+          name: 'author',
+          content: 'sohamtamboli',
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: 'https://www.anjalibusinessgroup.com',
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'Anjali Goat Farms',
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            'Goat Farming is a business which can be achieved with the help of less investment and also less space. In closed goat farming, the fodder required to feed the goats is given to them in the cowshed. Also, goats require less amount of food compared to other animals.',
+        },
+        {
+          hid: 'og:type',
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          hid: 'og:site_name',
+          property: 'og:site_name',
+          content: 'Anjali Goat Farms',
+        },
+        {
+          hid: 'og:locale',
+          property: 'og:locale',
+          content: 'en_us',
+        },
+        {
+          hid: 'twitter:card',
+          name: 'twitter:card',
+          content: 'summary',
+        },
+        {
+          hid: 'twitter:site',
+          name: 'twitter:site',
+          content: '@anjalibusinessgroup',
+        },
+        {
+          hid: 'twitter:creator',
+          name: 'twitter:creator',
+          content: '@reccemedia',
+        },
+        {
+          hid: 'twitter:url',
+          name: 'twitter:url',
+          content: 'https://www.anjalibusinessgroup.com/',
+        },
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: 'Anjali Goat Farms',
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content:
+            'Goat Farming is a business which can be achieved with the help of less investment and also less space. In closed goat farming, the fodder required to feed the goats is given to them in the cowshed. Also, goats require less amount of food compared to other animals.',
+        },
+      ],
+      link: [
+        {
+          rel: 'author',
+          href: 'https://github.com/sohamtamboli',
+        },
+        {
+          rel: 'canonical',
+          href: '//anjalibusinessgroup.com/',
+        },
+      ],
+    }
+  },
 }
 </script>
 
