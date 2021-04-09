c3ea49d7f56effe594c13e390498470b3be85dd4
Pruthvi Shetty <pruthvishetty5656@gmail.com>
2021-04-09 15:06:34 +0530

    Added bottom image

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,30 +1,32 @@
 <template>
-  <v-container class="contact__grid__container">
-    <!-- <h1 class="text-center mt-4 mb-6">Contact Us</h1> -->
-    <Heading title="Contact Us" class="mt-4" />
-    <Form />
-    <v-snackbar
-      v-model="snackbar"
-      :multi-line="multiLine"
-      :timeout="timeout"
-      :color="color"
-      light
-      bottom
-      elevation="3"
-      style="padding-bottom: 10px"
-    >
-      <span class="white--text">{{ text }}</span>
+  <div class="page__wrapper">
+    <v-container class="contact__grid__container">
+      <!-- <h1 class="text-center mt-4 mb-6">Contact Us</h1> -->
+      <Heading title="Contact Us" class="mt-4" />
+      <Form />
+      <v-snackbar
+        v-model="snackbar"
+        :multi-line="multiLine"
+        :timeout="timeout"
+        :color="color"
+        light
+        bottom
+        elevation="3"
+        style="padding-bottom: 10px"
+      >
+        <span class="white--text">{{ text }}</span>
 
-      <template v-slot:action="{ attrs }">
-        <v-btn color="white" text v-bind="attrs" @click="snackbar = false">
-          Close
-        </v-btn>
-      </template>
-    </v-snackbar>
-    <section class="page__section">
-      <div class="spacer" />
-    </section>
-  </v-container>
+        <template v-slot:action="{ attrs }">
+          <v-btn color="white" text v-bind="attrs" @click="snackbar = false">
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar>
+      <section class="page__section">
+        <div class="spacer" />
+      </section>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -56,7 +58,7 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
 .contact__grid__container {
   display: grid;
   place-items: center;
@@ -70,5 +72,18 @@ export default {
 .spacer {
   height: 12px;
   margin: 6rem 0;
+}
+.page__wrapper {
+  background-color: #efefef;
+  background-image: url('https://unsplash.com/photos/izzit01AX_U/download?w=2400');
+  background-repeat: no-repeat;
+  background-position: bottom;
+
+  @media only screen and (max-width: 600px) {
+    background-image: url('https://unsplash.com/photos/QSK3gaRlq3Y/download?w=640');
+    background-repeat: no-repeat;
+    background-position: bottom;
+    background-color: #fafafa;
+  }
 }
 </style>
