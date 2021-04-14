9732bae34670d23f4fbe300af34e9420f45f38fa
Soham Tamboli <sohamtamboli@gmail.com>
2021-04-15 03:32:02 +0530

    Added form validation

--- components/Register.vue
+++ components/Register.vue
@@ -27,10 +27,12 @@
           required
         />
         <v-text-field
+          ref="aadhar"
           v-model="aadhar"
           :rules="aadharRules"
           label="Aadhar Card Number"
           required
+          @keyup="addSpace"
         />
         <v-radio-group v-model="landAreaType" :rules="landAreaTypeRules" row>
           <template v-slot:label>
@@ -72,15 +74,15 @@
               farming? :
             </div>
           </template>
-          <v-radio label="Yes" value="true"></v-radio>
-          <v-radio label="No" value="false"></v-radio>
+          <v-radio label="Yes" value="yes"></v-radio>
+          <v-radio label="No" value="no"></v-radio>
         </v-radio-group>
         <v-radio-group v-model="haveShed" :rules="haveShedRules" row>
           <template v-slot:label>
             <div class="radio__label">Do you have Shed area? :</div>
           </template>
-          <v-radio label="Yes" value="true"></v-radio>
-          <v-radio label="No" value="false"></v-radio>
+          <v-radio label="Yes" value="yes"></v-radio>
+          <v-radio label="No" value="no"></v-radio>
         </v-radio-group>
         <v-slide-y-reverse-transition>
           <v-text-field
@@ -148,18 +150,23 @@ export default {
     ],
     mobile: '',
     mobileRules: [
-      (v) => !!v || 'Mobile number is required',
+      (v) => !!v || 'Mobile Number is required',
       (v) =>
-        /^(\+\d{1,3}[- ]?)?\d{10}$/.test(v) || 'Mobile number must be valid',
+        /^(\+\d{1,3}[- ]?)?\d{10}$/.test(v) || 'Mobile Number must be valid',
     ],
     email: '',
     emailRules: [(v) => /.+@.+\..+/.test(v) || 'E-mail must be valid'],
     aadhar: '',
-    aadharRules: [],
+    aadharRules: [
+      (v) => !!v || 'Aadhar Card Number is required',
+      (v) =>
+        /^[2-9]{1}[0-9]{3}\s[0-9]{4}\s[0-9]{4}$/.test(v) ||
+        'Aadhar Card Number must be valid',
+    ],
     landAreaType: null,
-    landAreaTypeRules: [],
+    landAreaTypeRules: [(v) => !!v || 'Land Area type is required'],
     landArea: '',
-    landAreaRules: [],
+    landAreaRules: [(v) => !!v || 'Land Area is required'],
     govValueLand: '',
     govValueLandRules: [(v) => !!v || 'This is required'],
     addressLineOne: '',
@@ -172,26 +179,35 @@ export default {
     ],
     education: '',
     educationRules: [(v) => !!v || 'Education is required'],
-    haveTrained: '',
-    haveTrainedRules: [],
-    haveShed: '',
-    haveShedRules: [],
+    haveTrained: null,
+    haveTrainedRules: [(v) => !!v || 'This is required'],
+    haveShed: null,
+    haveShedRules: [(v) => !!v || 'This is required'],
     shedArea: '',
-    shedAreaRules: [],
+    shedAreaRules: [(v) => !!v || 'Shed Area is required'],
     invest: '',
-    investRules: [(v) => /^[0-9]+$/.test(v) || 'This must be number'],
+    investRules: [
+      (v) => !!v || 'This is required',
+      (v) => /^[0-9]+$/.test(v) || 'This must be number',
+    ],
     beneficiary: '',
-    beneficiaryRules: [],
+    beneficiaryRules: [(v) => !!v || "Beneficiary's Name is required"],
     bankName: '',
-    bankNameRules: [],
+    bankNameRules: [(v) => !!v || 'Bank Name is required'],
     bankAccNo: '',
-    bankAccNoRules: [],
+    bankAccNoRules: [
+      (v) => !!v || 'Bank Account Number is required',
+      (v) => /^\d{9,18}$/.test(v) || 'Bank Account Number must be a valid',
+    ],
     ifsc: '',
-    ifscRules: [],
+    ifscRules: [
+      (v) => !!v || 'IFSC Code is required',
+      (v) => /^[A-Z]{4}0[A-Z0-9]{6}$/.test(v) || 'IFSC Code must be a valid',
+    ],
   }),
   computed: {
     checkShed() {
-      if (this.haveShed === 'true') {
+      if (this.haveShed === 'yes') {
         return true
       } else {
         return false
@@ -200,28 +216,52 @@ export default {
   },
 
   methods: {
+    addSpace() {
+      let em = this.$refs.aadhar.value
+      if (em === '') {
+        return
+      } else {
+        em = em.split(' ').join('')
+      }
+
+      const formatted = em.match(/.{1,4}/g).join(' ')
+      this.$refs.aadhar.value = formatted
+    },
     validate() {
       if (this.$refs.form.validate()) {
         this.submitForm()
         this.$refs.form.reset()
       }
     },
-    async submitForm() {
+    submitForm() {
       const payload = {
         name: this.name,
         mobile: this.mobile,
         email: this.email,
+        aadhar: this.aadhar,
+        landAreaType: this.landAreaType,
+        landArea: this.landArea,
+        govValueLand: this.govValueLand,
         address: this.addressLineOne + ' ' + this.addressLineTwo,
-        city: this.city,
-        state: this.state,
-        pincode: this.pincode,
+        age: this.age,
+        education: this.education,
+        haveTrained: this.haveTrained,
+        haveShed: this.haveShed,
+        shedArea: this.shedArea,
+        invest: this.invest,
+        beneficiary: this.beneficiary,
+        bankName: this.bankName,
+        bankAccNo: this.bankAccNo,
+        ifsc: this.ifsc,
       }
       try {
         // eslint-disable-next-line no-unused-vars
-        const resp = await this.$axios.$post(
-          `https://goatfarming-backend.herokuapp.com/api/contact/`,
-          payload
-        )
+        // const resp = await this.$axios.$post(
+        //   `https://goatfarming-backend.herokuapp.com/api/contact/`,
+        //   payload
+        // )
+        // eslint-disable-next-line no-console
+        console.log(JSON.stringify(payload, null, 2))
         this.$nuxt.$emit('showSuccessSnackbar')
       } catch (error) {
         this.$nuxt.$emit('showErrorSnackbar')
